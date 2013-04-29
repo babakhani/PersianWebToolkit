532a612a251c03d6c111b0f5f695cf54b991211f
reza babakhani <babakhani.reza@gamil.com>
2013-04-29 15:30:45 +0430

    Bugy

--- www/plugin/calendar/js/page.week.js
+++
@@ -1,9 +0,0 @@
-var Class_WeekPage = {
-    
-};
-var WeekPage = function(stage) {
-    inherit(this, [Class_Sprite, Class_DateRange,View_WeekPage,Class_WeekPage, {
-        stage : stage
-    }]);
-    return this;
-};
