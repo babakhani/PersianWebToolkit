505c110a48bccc0a65687cfc76569ba9cff9d342
reza babakhani <babakhani.reza@gamil.com>
2013-05-14 01:44:58 +0430

    Add Hedaer Menu

---
+++ jade/includes/head.script.html
@@ -0,0 +1,2 @@
+<script src="js/jquery.js"></script>
+<script src="js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+   
+  var href =  window.location.href;
+   
+   
+   $("#pageHeaderMenu li a").each(function(){
+      var text =$(this).attr("projectName"),    
+      self = $(this);
+      if(href.indexOf(text) !== -1){
+            self.parent().addClass('current');        
+      }
+   });
+});
