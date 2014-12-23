ea9361a7a1aa94518b66d3ee6f4a0dc00434c8f2
reza babakhani <babakhani.reza@gamil.com>
2014-12-23 16:49:32 +0330

    Fix favicon refrence

--- js/main.js
+++ js/main.js
@@ -9,4 +9,35 @@ $(document).ready(function () {
     });
     $("pre").addClass("prettyprint");
     prettyPrint();
+
+    function loadResource(filename, filetype) {
+        if (filetype == "js") {
+            var fileref = document.createElement('script')
+            fileref.setAttribute("type", "text/javascript")
+            fileref.setAttribute("src", filename)
+        } else if (filetype == "css") { //if filename is an external CSS file
+            var fileref = document.createElement("link")
+            fileref.setAttribute("rel", "stylesheet")
+            fileref.setAttribute("type", "text/css")
+            fileref.setAttribute("href", filename)
+        }
+
+        if (typeof fileref != "undefined")
+            document.getElementsByTagName("head")[0].appendChild(fileref)
+    }
+
+
+    $('select.theme-roller').change(function () {
+        var theme = $(this).val();
+        if (theme == 'dark') {
+            loadResource("http://rawgithub.com/babakhani/pwt.datepicker/Develop/dist/css/theme/persian-datepicker-dark.css", 'css')
+        }
+        if (theme == 'blue') {
+            loadResource("http://rawgithub.com/babakhani/pwt.datepicker/Develop/dist/css/theme/persian-datepicker-blue.css", 'css')
+        }
+        if (theme == 'default') {
+            loadResource("http://rawgithub.com/babakhani/pwt.datepicker/Develop/dist/css/persian-datepicker-0.3.5.min.css", 'css')
+        }
+    });
+
 });
