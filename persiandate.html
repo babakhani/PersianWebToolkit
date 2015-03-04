da0e84460d54ac59e55b1f0f7032d92cbe512a70
r.babakhani <babakahni.reza@gmail.com>
2015-03-04 16:04:24 +0330

    Remove doc before 0.3.6, clear folders, add vendor, add angular and doc of 0.3.7

--- persiandate.html
+++ persiandate.html
@@ -6,6 +6,5 @@
     <title>Document</title>
 </head>
 <body>
-
 </body>
 </html>
---
+++ vendor/js/directive.js
@@ -0,0 +1,15 @@
+
+
+  myApp.directive('script', function() {
+    return {
+      restrict: 'E',
+      scope: false,
+      link: function(scope, elem, attr) {
+        if (attr.type === 'text/javascript-lazy') {
+          var code = elem.text();
+          var f = new Function(code);
+          f();
+        }
+      }
+    };
+  });
