e7ce9c22d38b4a46945b609da02661cecc053f13
reza bbakhani <babakhani.reza@gmail.com>
2017-02-28 02:34:14 +0330

    add datepicker example, chgangelog, and code documentation

---
+++ docs/js/datepicker-example.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    $('.normal-example').persianDatepicker({});
+
+    $('.inline-example').persianDatepicker();
+
+    $('.format-example').persianDatepicker({
+        format: 'LLLL'
+    });
+
+    $('.formatter-example').persianDatepicker({
+        formatter: function(unix){
+            return 'selected unix: ' + unix;
+        }
+    });
+
+    $('.alt-field-example').persianDatepicker({
+        altField: '.datepicker-demo-alt.alt-field-example'
+    });
+
+    $('.persian-digit-example').persianDatepicker({
+        persianDigit: false
+    });
+
+    $('.view-mode-example').persianDatepicker({
+        viewMode: 'year'
+    });
+
+    $('.min-date-example').persianDatepicker({
+        minDate: new persianDate().unix()
+    });
+
+    $('.max-date-example').persianDatepicker({
+        maxDate: new persianDate().unix()
+    });
+
+    $('.check-date-example').persianDatepicker({
+        checkDate: function (unix) {
+            return new persianDate(unix).weekDayNumber != 4;
+        }
+    });
+
+    $('.check-month-example').persianDatepicker({
+        checkMonth: function (month) {
+            return month < 6;
+        }
+    });
+
+    $('.only-timepicker-example').persianDatepicker({
+        onlyTimePicker: true
+    });
+
+    $('.auto-close-example').persianDatepicker({
+        autoClose: true
+    });
+
+    $('.on-select-example').persianDatepicker({
+        onSelect: function(unix){
+            alert('datepicker select : ' + unix);
+        }
+    });
+
+});
